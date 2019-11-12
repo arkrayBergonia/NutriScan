02f0e86c2d352a26ffb83f5e5399d4191905177d
arkray - Francis Bergonia <fbergonia.arkray@gmail.com>
2019-11-12 10:39:44 +0800

    Initial Commit

---
+++ NutriSCAN/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NutriSCAN
+//
+//  Created by Francis Jemuel Bergonia on 11/12/19.
+//  Copyright © 2019 Arkray Marketing, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
